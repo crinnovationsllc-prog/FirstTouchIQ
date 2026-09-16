747353b5fc633ad4969e17684ce79c9e7100d795
crinnovationsllc-prog <crinnovationsllc@gmail.com>
2026-09-15 21:06:44 -1000

    Create page.tsx

---
+++ app/parent/page.tsx
@@ -0,0 +1,8 @@
+export default function ParentPage() {
+  return (
+    <main style={{ padding: "24px" }}>
+      <h1>FirstTouchIQ Parent Portal</h1>
+      <p>Parent registration is coming soon.</p>
+    </main>
+  );
+}
